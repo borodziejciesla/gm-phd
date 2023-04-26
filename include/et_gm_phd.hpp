e9ff9cdac2981c15918f23fa7cfcd4c18a2b56b8
Maciej Różewicz <rozewicz.maciej@gmail.com>
2023-04-27 01:03:25 +0200

    Added initialized partitioning

--- include/et_gm_phd.hpp
+++ include/et_gm_phd.hpp
@@ -17,6 +17,12 @@ namespace mot {
   template <size_t state_size, size_t measurement_size>
   class EtGmPhd {
     public:
+    using StateSizeVector = Eigen::Vector<double, state_size>;
+      using StateSizeMatrix = Eigen::Matrix<double, state_size, state_size>;
+      using MeasurementSizeVector = Eigen::Vector<double, measurement_size>;
+      using MeasurementSizeMatrix = Eigen::Matrix<double, measurement_size, measurement_size>;
+
+      using Measurement = ValueWithCovariance<measurement_size>;
       using Object = ExtentState<state_size>;
 
     public:
@@ -62,10 +68,56 @@ namespace mot {
         StateSizeVector state = StateSizeVector::Zero();
         StateSizeMatrix covariance = StateSizeMatrix::Zero();
       };
-      
+
       std::vector<Object> objects_;
 
     private:
+      void CalculateDistances(const std::vector<Measurement> & measurements) {
+        // Clear matrix
+        for (auto & row : distance_matrix_)
+          row.clear();
+        distance_matrix_.clear();
+
+        // Allocate new matrix
+        distance_matrix_ = DistanceMatrix(measurements.size());
+        for (auto & row : distance_matrix_)
+          row = std::vector(measurements.size());
+        
+        // Calculate distances
+        for (auto row_index = 0u; row_index < measurements.size(); row_index++) {
+          for (auto col_index = row_index; col_index < measurements.size(); col_index++) {
+            const auto distance = CalculateMahalanobisDistance(measurements.at(row_indes), measurements.at(col_index));
+            distance_matrix_.at(row_index).at(col_index) = distance;
+            distance_matrix_.at(col_index).at(row_index) = distance;
+          }
+        }
+      }
+
+      void MakeDistancePartitioning(const std::vector<Measurement> & measurements) {
+        // Prepare distance matrix
+        CalculateDistances(measurements);
+        // Main partitioning loop
+      }
+
+      void FindNeihgbours(const uint32_t i, const std::vector<Measurement> & measurements) {
+        for (auto j = 0u; j <= measurements.size(); j++) {
+          if ((j != i)) {
+            //
+          }
+        }
+      }
+
+      static float CalculateMahalanobisDistance(const Measurement & z_i, const Measurement & z_j) {
+        const auto diff = z_i.value - z_j.value;
+        const auto covariance = z_i.covariance + z_j.covariance;
+
+        const auto distance_raw = diff.transpose() * covariance.inverse() * diff;
+        return distance_raw(0u);
+      }
+
+      using DistanceMatrix = std::vector<std::vector<float>>;
+
+      DistanceMatrix distance_matrix_;
   };
 } //  namespace mot
 
