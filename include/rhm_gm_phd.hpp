4fc0792e03ad338d4e425d988749afa11454beac
Maciej Rozewicz <rozewicz.maciej@gmail.com>
2023-12-31 01:54:25 +0100

    Updated rhm

--- include/rhm_gm_phd.hpp
+++ include/rhm_gm_phd.hpp
@@ -18,6 +18,8 @@ namespace mot {
   constexpr auto kinematic_state_size = 4u;
   constexpr auto extend_state_size = 3u;
   constexpr auto measurement_size = 2u;
+  constexpr auto nx = extend_state_size + kinematic_state_size;
+  constexpr auto augmented_size = nx + 1u + measurement_size;
 
   struct Partition {
     std::vector<int> points;
@@ -25,25 +27,31 @@ namespace mot {
 
   class RhmGmPhd {
     public:
-      using KinematicStateSizeVector = Eigen::Vector<float, kinematic_state_size>;
-      using KinematicStateSizeMatrix = Eigen::Matrix<float, kinematic_state_size, kinematic_state_size>;
-      using ExtendStateSizeVector = Eigen::Vector<float, extend_state_size>;
-      using ExtendStateSizeMatrix = Eigen::Matrix<float, extend_state_size, extend_state_size>;
-      using MeasurementSizeVector = Eigen::Vector<float, measurement_size>;
-      using MeasurementSizeMatrix = Eigen::Matrix<float, measurement_size, measurement_size>;
-      using SensorPoseVector = Eigen::Vector<float, 3u>;
-      using SensorPoseMatrix = Eigen::Matrix<float, 3u, 3u>;
+      using KinematicStateSizeVector = Eigen::Vector<double, kinematic_state_size>;
+      using KinematicStateSizeMatrix = Eigen::Matrix<double, kinematic_state_size, kinematic_state_size>;
+      using ExtendStateSizeVector = Eigen::Vector<double, extend_state_size>;
+      using ExtendStateSizeMatrix = Eigen::Matrix<double, extend_state_size, extend_state_size>;
+      using MeasurementSizeVector = Eigen::Vector<double, measurement_size>;
+      using MeasurementSizeMatrix = Eigen::Matrix<double, measurement_size, measurement_size>;
+      using SensorPoseVector = Eigen::Vector<double, 3u>;
+      using SensorPoseMatrix = Eigen::Matrix<double, 3u, 3u>;
+      using AugmentedVector = Eigen::Vector<double, augmented_size>;
+      using AugmentedMatrix = Eigen::Matrix<double, augmented_size, augmented_size>;
 
-      using ObservationMatrix = Eigen::Matrix<float, measurement_size, measurement_size>;
-      using TransitionMatrix = Eigen::Matrix<float, kinematic_state_size, kinematic_state_size>;
+      using ObservationMatrix = Eigen::Matrix<double, measurement_size, measurement_size>;
+      using TransitionMatrix = Eigen::Matrix<double, kinematic_state_size, kinematic_state_size>;
 
       using Measurement = ValueWithCovariance<measurement_size>;
 
       using Hypothesis = struct {
-        float weight = 0.0f;
+        double weight = 0.0;
         
         ValueWithCovariance<kinematic_state_size> kinematic;
         ValueWithCovariance<extend_state_size> extend;
+
+        double s = 0.0;
+        double var_s = 0.0;
+        double var_zero = 0.0;
       };
       using HypothesisList = std::vector<Hypothesis>;
 
@@ -55,6 +63,14 @@ namespace mot {
       };
       using ObjectsList = std::vector<Object>;
 
+      using UtPoints = struct {
+        std::vector<Eigen::Vector<double, augmented_size>> x_pts;
+        std::vector<double> wm;
+        std::vector<double> wc;
+        size_t points_number = 0u;
+        std::vector<double> z_pts;
+      };
+
     public:
       RhmGmPhd();
       ~RhmGmPhd(void);
@@ -65,13 +81,17 @@ namespace mot {
     private:
       void MakeBirth(void);
       void MakePrediction(void);
+      void MakePartitioning(void);
       void MakeCorrection(const std::vector<Measurement>& measurements);
       void MakePruning(void);
       void MakeMerging(void);
       void ExtractObjects(void);
+      void ProcessMeasurementWithHypothesis(const Hypothesis& hypothesis, const Measurement& measurement);
+      UtPoints GetUkfPointsAndWeights(const AugmentedVector& mu_a, const AugmentedMatrix& c_a, const double theta, const Measurement& measurement);
+      AugmentedVector UtCorrectionStep(const UtPoints& ut_points);
 
       double prev_timestamp_ = 0.0;
-      float time_delta_ = 0.0f;
+      double time_delta_ = 0.0;
 
       HypothesisList hypothesis_list_;
       HypothesisList predicted_hypothesis_list_;
@@ -85,9 +105,23 @@ namespace mot {
 
       KinematicStateSizeMatrix q_;
 
-      float gamma_ = 1e-5f;
-      float pd_ = 0.9f;
-      float ps_ = 0.9f;
+      Eigen::Vector<double, nx> mu_ = Eigen::Vector<double, nx>::Zero();
+      Eigen::Matrix<double, nx, nx> px_ = Eigen::Matrix<double, nx, nx>::Zero();
+      Eigen::Vector2d m_ = Eigen::Vector2d::Zero();
+      double pl_ = 1.0;
+
+      double gamma_ = 1e-5;
+      double pd_ = 0.9;
+      double ps_ = 0.9;
+
+      const double alpha_ = 1.0;
+      const double beta_ = 2.0;
+      const double kappa_ = 0.0;
+
+      double s_ = 0.0;
+      double z_mean_ = 0.0;
+
+      int jk_ = 0u;
   };
 } //  namespace mot
 
--- src/rhm_gm_phd.cpp
+++ src/rhm_gm_phd.cpp
@@ -1,14 +1,17 @@
 #include "rhm_gm_phd.hpp"
 
+#include <iterator>
 #include <random>
 
 namespace mot {
+  constexpr auto pi = 3.141592653589793;
+
   RhmGmPhd::RhmGmPhd() {};
 
   RhmGmPhd::~RhmGmPhd(void) = default;
 
   void RhmGmPhd::Run(const double timestamp, const std::vector<Measurement>& measurements) {
-    time_delta_ = static_cast<float>(timestamp - prev_timestamp_);
+    time_delta_ = static_cast<double>(timestamp - prev_timestamp_);
     prev_timestamp_ = timestamp;
 
     predicted_hypothesis_list_.clear();
@@ -45,9 +48,9 @@ namespace mot {
 
       birth_hypothesis.kinematic.covariance = KinematicStateSizeMatrix::Identity();
 
-      birth_hypothesis.extend.value(0u) = 0.5f;
-      birth_hypothesis.extend.value(1u) = 0.5f;
-      birth_hypothesis.extend.value(2u) = 0.5f;
+      birth_hypothesis.extend.value(0u) = 0.5;
+      birth_hypothesis.extend.value(1u) = 0.5;
+      birth_hypothesis.extend.value(2u) = 0.5;
 
       birth_hypothesis.extend.covariance = ExtendStateSizeMatrix::Identity();
 
@@ -63,7 +66,7 @@ namespace mot {
 
         const KinematicStateSizeVector predicted_kinematic_state = transition_matrix_ * hypothesis.kinematic.value;
         const KinematicStateSizeMatrix predicted_kinematic_covariance = transition_matrix_ * hypothesis.kinematic.covariance * transition_matrix_.transpose() + q_;
-        const ValueWithCovariance<kinematic_state_size> predicted_kinematic= {
+        const ValueWithCovariance<kinematic_state_size> predicted_kinematic = {
           predicted_kinematic_state,      //  value
           predicted_kinematic_covariance  // kovariance
         };
@@ -79,6 +82,11 @@ namespace mot {
         return predicted_hypothesis;
       }
     );
+    jk_ = predicted_hypothesis_list_.size();
+  }
+
+  void RhmGmPhd::MakePartitioning(void) {
+    //
   }
 
   void RhmGmPhd::MakeCorrection(const std::vector<Measurement>& measurements) {
@@ -89,7 +97,7 @@ namespace mot {
       std::back_inserter(predicted_hypothesis_list_),
       [this](const Hypothesis& hypothesis) -> Hypothesis {
         Hypothesis new_hypothesis = {
-          .weight = 1.0f - (1.0f - std::exp(-gamma_)) * pd_ * hypothesis.weight,
+          .weight = 1.0 - (1.0 - std::exp(-gamma_)) * pd_ * hypothesis.weight,
           .kinematic = hypothesis.kinematic,
           .extend = hypothesis.extend
         };
@@ -100,14 +108,138 @@ namespace mot {
     // Go over partitioning
     auto l = 0u;
     for (const auto & partition : partitions_) {
-      // for (const auto & point : partition.points) {
+      l++;
       for (auto j = 0; j < hypothesis_list_.size(); j++) {
-        // TODO
-        for (auto nz = 0u; nz < partition.points.size(); nz++) {
+        // Line 10
+        px_.block<extend_state_size, extend_state_size>(0u, 0u) = hypothesis_list_.at(j).extend.covariance;
+        px_.block<kinematic_state_size, kinematic_state_size>(extend_state_size, extend_state_size) = hypothesis_list_.at(j).kinematic.covariance;
+        mu_.head<extend_state_size>() = hypothesis_list_.at(j).extend.value;
+        mu_.tail<kinematic_state_size>() = hypothesis_list_.at(j).kinematic.value;
+        m_ << mu_(3u), mu_(4u);
+        pl_ = 1.0;
 
+        for (auto nz = 0u; nz < partition.points.size(); nz++) {
+          ProcessMeasurementWithHypothesis(hypothesis_list_.at(j), measurements.at(partition.points.at(nz)));
         }
+
+        // Line 24, 25
+        pl_ *= std::exp(-gamma_) * pd_ * gamma_;
+        Hypothesis new_hypothesis;
+        new_hypothesis.weight = pl_;
+        new_hypothesis.kinematic.value = mu_.tail<kinematic_state_size>();
+        new_hypothesis.kinematic.covariance = px_.block<kinematic_state_size, kinematic_state_size>(extend_state_size, extend_state_size);
+        new_hypothesis.extend.value = mu_.head<extend_state_size>();
+        new_hypothesis.extend.covariance = px_.block<extend_state_size, extend_state_size>(0u, 0u);
+
+        predicted_hypothesis_list_.push_back(new_hypothesis);
+      }
+      // Line 27
+      auto dw = (partition.points.size() == 1) ? 1.0 : 0.0;
+      for (auto j = 0u; j < jk_; j++) {
+        dw += predicted_hypothesis_list_.at(j + jk_ * l).weight;
+      }
+
+      for (auto j = 0u; j < jk_; j++) {
+        predicted_hypothesis_list_.at(j + jk_ * l).weight /= dw;
       }
     };
+  }
+
+  void RhmGmPhd::ProcessMeasurementWithHypothesis(const Hypothesis& hypothesis, const Measurement& measurement) {
+    // Line 12
+    Eigen::Vector<double, augmented_size> mu_a = Eigen::Vector<double, augmented_size>::Zero();
+    mu_a.head<nx>() = mu_;
+    mu_a(nx) = 0.0;//s?
+    Eigen::Matrix<double, augmented_size, augmented_size> c_a = Eigen::Matrix<double, augmented_size, augmented_size>::Identity();
+    c_a.block<nx, nx>(0u, 0u) = px_;
+    c_a(nx, nx) = 1.0;// sigma_s
+    c_a.block<measurement_size, measurement_size>(nx + 1u, nx + 1u) = measurement.covariance;
+    // Line 13
+    const Eigen::Vector2d diff = measurement.value - m_;
+    const auto dx = diff(0u) * std::cos(mu_(3u)) - diff(1u) * std::sin(mu_(3u));
+    const auto dy = diff(0u) * std::sin(mu_(3u)) + diff(1u) * std::cos(mu_(3u));
+    const auto theta = std::atan2(dy, dx);
+    // Line 14, 15
+    const auto ut_points = GetUkfPointsAndWeights(mu_a, c_a, theta, measurement);
+    // Line 16, 17, 18, 19
+    const auto k = UtCorrectionStep(ut_points);
+    // line 20
+    mu_a += k * (0.0f - z_mean_);
+    c_a -= k * s_ * k.transpose();
+
+    px_.block<extend_state_size, extend_state_size>(0u, 0u) = c_a.block<extend_state_size, extend_state_size>(0u, 0u);
+    px_.block<kinematic_state_size, kinematic_state_size>(extend_state_size, extend_state_size) = c_a.block<kinematic_state_size, kinematic_state_size>(extend_state_size, extend_state_size);
+    mu_.head<extend_state_size>() = mu_a.head<extend_state_size>();
+    mu_.tail<kinematic_state_size>() = mu_a.tail<kinematic_state_size>();
+
+    pl_ *= (1.0 / (std::sqrt(s_ * 2.0 * pi))) * std::exp(-0.5 * z_mean_ * s_ * z_mean_);
+  }
+
+  RhmGmPhd::UtPoints RhmGmPhd::GetUkfPointsAndWeights(const AugmentedVector& mu_a, const AugmentedMatrix& c_a, const double theta, const Measurement& measurement) {
+    UtPoints ut_points;
+    const auto n = 5u;
+    const auto lambda = std::pow(alpha_, 2u) + (static_cast<double>(n) + kappa_)  -  static_cast<double>(n);
+
+    // Line 14
+    ut_points.wm.resize(2u * n + 1u);
+    ut_points.wm.at(0u) = lambda / (static_cast<double>(n) + lambda);
+    std::fill_n(std::next(ut_points.wm.begin(), 1), 2u * n, 1.0 / (2.0 * (static_cast<double>(n)  + lambda)));
+
+    ut_points.wc.resize(2u * n + 1u);
+    ut_points.wc.at(0u) = (lambda / (static_cast<double>(n) + lambda)) + (1 - std::pow(alpha_, 2.0) + beta_);
+    std::fill_n(std::next(ut_points.wc.begin(), 1), 2u * n, 1.0 / (2.0 * (static_cast<double>(n)  + lambda)));
+
+    const AugmentedMatrix chol = c_a.llt().matrixL();
+    const auto sigma_points_cholesky_part = std::sqrt(static_cast<double>(n) + lambda) * chol;
+
+    ut_points.x_pts.resize(2u * n + 1u);
+    ut_points.x_pts.at(0) = mu_a;
+    for (auto index = 0u; index < n; index++) {
+      const auto col_index = sigma_points_cholesky_part.col(index);
+      ut_points.x_pts.at(index + 1u) = mu_a + col_index;
+      ut_points.x_pts.at(n + index + 1u) = mu_a - col_index;
+    }
+
+    // Line 15
+    std::transform(ut_points.x_pts.begin(), ut_points.x_pts.end(),
+      std::back_inserter(ut_points.z_pts),
+      [theta, measurement, this](const Eigen::Vector<double, augmented_size>& mu_a) -> double {
+        const auto a = mu_a(0u);
+        const auto b = mu_a(1u);
+        const auto phi = mu_a(2u);
+        const auto s = mu_a(augmented_size);
+        const auto v = mu_a.tail<2u>();
+        Eigen::Vector2d e;
+        e << std::cos(theta), std::sin(theta);
+        Eigen::Vector2d m;
+        m << mu_a(kinematic_state_size), mu_a(kinematic_state_size + 1u);
+
+        const auto r = a * b / std::sqrt(std::pow(a * std::sin(theta - phi), 2.0) + std::pow(b * std::cos(theta - phi), 2.0));
+
+        double expected_pseudomeasurement = std::pow(s * r, 2)
+          + 2.0 * s * r * (e.transpose() * v).norm()
+          + v.norm()
+          + (measurement.value - m).norm();
+
+        return expected_pseudomeasurement;
+      }
+    );
+  }
+
+  RhmGmPhd::AugmentedVector RhmGmPhd::UtCorrectionStep(const RhmGmPhd::UtPoints& ut_points) {
+    z_mean_ = 0.0;
+    for (auto index = 0u; index < ut_points.points_number; index++)
+      z_mean_ += ut_points.wm.at(index) * ut_points.z_pts.at(index);
+
+    s_ = 0.0;
+    AugmentedVector p = AugmentedVector::Zero();
+    for (auto index = 0u; index < ut_points.points_number; index++) {
+      s_ += ut_points.wc.at(index) * std::pow(ut_points.z_pts.at(index), 2);
+      p += ut_points.wc.at(index) * ut_points.x_pts.at(index) * ut_points.z_pts.at(index);
+    }
+
+    const auto k = p / s_;
+    return k;
   }
 
   void RhmGmPhd::MakePruning(void) {
